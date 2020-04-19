84e0dfdca22cbe5fb62ea7d499a02c0d8c1f5136
Ricky Su <100083700@students.swinburne.edu.my>
2020-04-19 16:47:43 +0800

    Update nav_header.php

--- includes/nav_header.php
+++ includes/nav_header.php
@@ -3,31 +3,29 @@
 	//start session
 	//session_start();
 	
-	echo '<div class="navbar navbar-default navbar-fixed-top" id="topnav">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a href="products.php" class="navbar-brand">PCSects</a>
-				</div>';
-		
-			//fetch session for login_id
-			if(isset($_SESSION['login_id']))
-			{
-				$name = $_SESSION["first_name"];
-				
-				echo '<ul class="nav navbar-nav navbar-right">
-					<li><a href="accountsettings.php">Update Account</a></li>
-					<li><a href="logout.php">Sign Out</a></li>
-				</ul>';
-				$price_discount = 0.5;
-			}
-			else
-			{
-				$price_discount = 1;
-			echo '<ul class="nav navbar-nav navbar-right">
-					<li><a href="login.php">Sign In</a></li>
-					<li><a href="registration.php">Sign Up</a></li>
-				</ul>';
-			}
-		echo '</div>
-	</div>'
+	echo '<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">CraftPopHouse</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+	  <li><a href="#">Page 3</a></li>
+    </ul>
+    
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+	<form class="navbar-form navbar-right" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search" name="search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+  </div>
+</nav>';
+
 ?>
