6f70e8a9d00098b1b6e29244ce6ebcc7df021630
Ricky Su <100083700@students.swinburne.edu.my>
2020-04-20 09:41:30 +0800

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -9,7 +9,7 @@
 	<meta name="author" content="Ricky Su"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link href="styles/homepagestyle.css" rel="stylesheet" type="text/css">
+	<link href="styles/contact.css" rel="stylesheet" type="text/css">
 	
 	<!--Bootstrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -28,7 +28,10 @@
 		include "includes/nav_header.php";
 	?>
 	
-	
+	<div class="box">
+		<img src="images/contact.png" alt="image" style="width:100%;">
+		<div class="contact">Contact Us</div>
+	</div>
 	
 	<?php
 		include "includes/footer.php";
