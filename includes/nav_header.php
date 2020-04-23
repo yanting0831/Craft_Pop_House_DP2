17fbe85b4ba4689f4dfd867ca3a4c7d6743e30dc
Yan Ting <101209183@students.swinburne.edu.my>
2020-04-24 00:41:21 +0800

    Update nav_header.php

--- includes/nav_header.php
+++ includes/nav_header.php
@@ -4,7 +4,7 @@
 	echo '<nav class="navbar navbar-inverse">
 			<div class="container-fluid">
 				<div class="navbar-header">
-				<a class="navbar-brand" href="index.php">CraftPopHouse</a>
+				<a class="navbar-brand" href="index.php">Craft Pop House</a>
 				</div>
 				<ul class="nav navbar-nav">
 					<li class="active"><a href="index.php">Home</a></li>
