97847198bd6b5ea52120bfa458c4ea4de0689a3f
Yan Ting <101209183@students.swinburne.edu.my>
2020-04-24 00:47:32 +0800

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="Web Development">
 	<meta name="keywords" content="HTML, CSS, JavaScript">
 	<meta name="author" content="Eric Kong">
-	<link rel="stylesheet" type="text/css" href="styles/register.css">
+	<link rel="stylesheet" type="text/css" href="styles/Login.css">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -18,46 +18,45 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
-
-	<?php
+<?php
 		include "includes/nav_header.php";
 	?>	
-	
-	<!--<div class="box">
-	<img src="images/avatar.jpg" id="avatar">
-		<h1>Login Account </h1>
-			<form>
-				<p>Email</p>
-				<input type="text" class="nameline "name="Uemail" required="required" placeholder="Enter Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
-				<p>Password</p>
-				<input type="password" class="Passwordline "name="Upassword" placeholder="Enter Password" required="required"/>
-				<p><input type="submit" name="submit" class="login"></p>
-				<a href="file:///D:/Xampp/htdocs/Craft_Pop_House_DP2/registration.html">Don't have an account?</a>
-			</form>
-	
-	</div>
-	-->
-	<form action="login.php" method="POST">
-	<div class="container">
-		<h1>Login Page</h1>
-		<label for="email"><b>Email address  (as login ID):</b></label>
-		<input type="email" placeholder="Enter Your Email Address" name="email">
+<div class="container-fluid bg" >
+
+	<div class="row">
+		<div class="col-md-4 col-sm-4 col-xs-12"></div>
+		<div class="col-md-4 col-sm-4 col-xs-12">
 		
-		<label for="password"><b>Password: </b></label>
-		<input type="password" name="password"></p>
-		
-		<input type="submit" class="registerbtn" value="Login" name="login_button">
+		<form class="form-container" method="POST" action="login.php">
+			<h1> Login </h1>
 			
-		<input type="reset" class="resetbtn" value="Reset" name="reset_button">
-		<?php echo display_error(); ?>
+			<?php echo display_error(); ?>
+			<div class="form-group">
+				<label for="exampleInputEmail1">Email address</label>
+				<input type="email" name="email"  class="form-control" required="required" id="exampleInputEmail1" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="exampleInputPassword1">Password</label>
+				<input type="password" name="password" required="required" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			</div>
+			<button type="submit" name="login_button" class="btn btn-success btn-block">Submit</button>
+			
+			
+		</form>
+		
 	</div>
-	
+	<div class="col-md-4 col-sm-4 col-xs-12"></div>
+	</div>
+	</div>
+
 	<?php
 		include "includes/footer.php";
-	?>	
-
+	?>
+	
 	
 </body>
+</html>
