dc016dd01a613eeafaf3f776044f9ff01645ca46
Ricky Su <100083700@students.swinburne.edu.my>
2020-04-23 09:40:51 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -63,8 +63,6 @@ function register(){
 
 			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
 			$_SESSION['success']  = "You are now logged in";*/
-			
-			header('location: registration.php');
 						
 		}
 	}
