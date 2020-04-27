39d2666d6717ecf05189616773475443ed2ed67f
Eric1231 <101208805@students.swinburne.edu.my>
2020-04-28 01:45:26 +0800

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,16 @@
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading"> 
+		<h3 class="panel-title">Product Categories </h3> 
+	</div>
+	
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			
+		<?php getCategory();?>
+		
+		
+		</ul>
+	</div>
+	
+	
+</div>
