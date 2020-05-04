e1c439cfbab7e8e5a9c20b76ecb3066f08d24a7f
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-04 14:01:33 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -173,7 +173,7 @@ function login(){
 
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
-				header('location: admin/users.php');
+				header('location: admin/Admin.php');
 					  
 			}
 			
