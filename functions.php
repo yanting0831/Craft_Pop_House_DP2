2fa4055f6298f1cdc6188c9431d3fd8e2012d33c
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-04 14:16:17 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -181,6 +181,7 @@ function login(){
 			{
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success'] = "You are now logged in";
+				header('location: seller/SellerProduct.php');
 			}
 			
 			else{
