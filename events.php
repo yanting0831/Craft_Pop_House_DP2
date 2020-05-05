1ef0c932b0e6a66e43edddce5ead8ddb955f917a
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-05 11:17:41 +0800

    Update events.php

--- events.php
+++ events.php
@@ -1,3 +1,7 @@
+<?php 
+	include('functions.php');
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -10,6 +14,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href="styles/event.css" rel="stylesheet" type="text/css">
+	<link href="styles/cart.css" rel="stylesheet" type="text/css">
 	
 	<!--Bootstrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
