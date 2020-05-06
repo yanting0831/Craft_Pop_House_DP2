7ea47e12e83ddaca8d3e28b93d8a1cea7f2904d7
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-06 18:16:50 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -2,7 +2,6 @@
 
 	//start session
 	session_start();
-
 	
 	require_once("includes/component.php");
 	require_once("function.php");
@@ -19,7 +18,87 @@
 			}
 		}
 	}
-?>
+	
+	//create database connection object
+	$conn = new mysqli('localhost','root','','cph');
+	
+	if($conn === false)
+	{
+		die ("Error: Could not connect. " . mysqli_connect_error());
+	}
+	
+	//select database
+	mysqli_select_db ($conn , 'cph') or die (mysqli_connect_error);
+	
+	//print_r ($_SESSION['cart']);
+	
+	$status_msg="";
+	$input_valid_flag = true;
+	//when add button is pressed
+	if (isset($_POST['update']))
+	{	
+		/*foreach($_SESSION['cart'] as $key => $values)
+		{
+			if($values['item_name'] == $product_name)
+			{	
+				$_SESSION['cart'][$key]['item_quantity'] = $_SESSION['cart'][$key]['item_quantity'] + $values['item_quantity'];
+			}
+			//echo $_SESSION['cart'][$key]['product_id'];
+			echo $_SESSION['cart'][$key]['item_quantity'];
+			//echo $values['item_quantity'];
+		}*/
+		
+		if($input_valid_flag == true)
+		{	
+			//sanitize the input data
+			$updateQty = mysqli_escape_string($conn, $_POST["updateQty"]);
+			
+			//Return the values from a single column in the input array
+			$product_id = array_column($_SESSION['cart'], 'product_id');
+			$item_quantity = array_column($_SESSION['cart'], 'item_quantity');
+			
+			//implode to convert array to string
+			$string_id = implode("", $product_id);
+			
+			//select data from database
+			$get_product= "select * from products";
+			$run_products= mysqli_query($connection,$get_product);
+			
+			if($row_products = mysqli_fetch_assoc($run_products))
+			{
+				foreach ($product_id as $id){
+					//echo $id."<br>";
+					if ($row_products['product_id'] == $id){
+						//echo $id."<br>";
+						//echo $row_products['product_id']."<br>";
+						//echo $product_id;
+						//update the database table content
+						$sql = "UPDATE products SET product_quantity='$updateQty' WHERE product_id='$id'";
+						
+						$result = mysqli_query($conn, $sql);
+			
+						if($result)
+						{
+							$status_msg = '<h3 style="color:green">Quantity Added!.</h3>';
+						}
+						else
+						{
+							$status_msg = "Error: " . mysql_error();
+						}	
+					}
+				}
+				
+				
+			}
+			//update the database table content
+			//$sql = "UPDATE products SET product_quantity='$quantity' WHERE product_id='$string_id'";
+			
+			//echo $string_id."<br>";
+			//echo $updateQty;
+			
+		}
+	}
+	?>
 
 <!DOCTYPE html>
 
@@ -58,11 +137,13 @@
 					<?php
 					$total = 0;
 					if (isset($_SESSION['cart'])){
+						
 						$product_id = array_column($_SESSION['cart'], 'product_id');
 						$item_quantity = array_column($_SESSION['cart'], 'item_quantity');
 						$product_price = array_column($_SESSION['cart'], 'product_price');
 						$product_name = array_column($_SESSION['cart'], 'item_name');
-
+						
+						//select data from database
 						$get_product= "select * from products";
 						$run_products= mysqli_query($connection,$get_product);
 						
@@ -76,13 +157,9 @@
 							}
 						}
 						
-						
-						
 						foreach($_SESSION["cart"] as $key => $value)
 						{
 							$total = $total + ($value["item_quantity"] * $value["product_price"]);
-							//echo $value['item_quantity'];
-							
 						}
 						
 					}
@@ -114,53 +191,46 @@
 							<hr>
 							<h6>Amount Payable</h6>
 							<hr>
-							<?php
-							foreach($item_quantity as $qty)
-							{
-								echo "Quantity per item respectively: $qty<br>";
-							}
-							
-							?>
 							<br>
 
-						<!-- Payment -->
-						<div id="paypal-button-container"></div>
+							<!-- Payment -->
+							<div id="paypal-button-container"></div>
 
-    					<!-- Include the PayPal JavaScript SDK -->
-					    <script src="https://www.paypal.com/sdk/js?client-id=AVJ4ZM21sgUqwIJ75Dc8t1-9RBATky7G59n_XlBKu_vJAtYM9lZNYIN2kajResP-8Hf7fJDZLJ7D_OuC&currency=MYR"></script>
+							<!-- Include the PayPal JavaScript SDK -->
+							<script src="https://www.paypal.com/sdk/js?client-id=AVJ4ZM21sgUqwIJ75Dc8t1-9RBATky7G59n_XlBKu_vJAtYM9lZNYIN2kajResP-8Hf7fJDZLJ7D_OuC&currency=MYR"></script>
 
-					    <script type="text/javascript">
-					    	var total = <?php echo json_encode($total); ?>;					    		
-					    </script>
+							<script type="text/javascript">
+								var total = <?php echo json_encode($total); ?>;					    		
+							</script>
 
-					    <script>
-					        // Render the PayPal button into #paypal-button-container
-					        paypal.Buttons({
-					            style: {
-					                layout: 'horizontal'
-					            },
-					            
-					            // Set up the transaction
-					            createOrder: function(data, actions) {
-					                return actions.order.create({
-					                    purchase_units: [{
-					                        amount: {
-					                            value: total
-					                        }
-					                    }]
-					                });
-					            },
+							<script>
+								// Render the PayPal button into #paypal-button-container
+								paypal.Buttons({
+									style: {
+										layout: 'horizontal'
+									},
+									
+									// Set up the transaction
+									createOrder: function(data, actions) {
+										return actions.order.create({
+											purchase_units: [{
+												amount: {
+													value: total
+												}
+											}]
+										});
+									},
 
-					            // Finalize the transaction
-					            onApprove: function(data, actions) {
-					                return actions.order.capture().then(function(details) {
-					                    // Show a success message to the buyer
-					                    alert('Transaction completed by ' + details.payer.name.given_name + '!');
-					                });
-					            }
-					            
-					        }).render('#paypal-button-container');
-					    </script>
+									// Finalize the transaction
+									onApprove: function(data, actions) {
+										return actions.order.capture().then(function(details) {
+											// Show a success message to the buyer
+											alert('Transaction completed by ' + details.payer.name.given_name + '!');
+										});
+									}
+									
+								}).render('#paypal-button-container');
+							</script>
 
 						</div>
 						
@@ -171,13 +241,52 @@
 							<h6>$<?php echo $total;?></h6>
 							<hr>
 						</div>
+						
 					</div>
 				</div>
 
 			</div>
+			<div class="details col-md-5 offset-md-1 table-bordered mt-5 bg-white h-25">
+				<p> Quantity </p>
+				<div class="pt-4">
+				<form action="cart.php" method="POST">
+					
+					<div class="col-md-12">
+						<?php
+							$connection = mysqli_connect('localhost','root','','cph');
+							
+							$get_products = "select * from products order by 1 DESC LIMIT 0,8";
+		
+							$run_products = mysqli_query($connection,$get_products);
+							
+							while($row_products = mysqli_fetch_array($run_products))
+							{
+								$product_quantity = $row_products['product_quantity'];
+								$product_name = $row_products['product_title'];
+								echo $product_name;
+								echo "<input class='form-control w-25 d-inline' type='text' name='updateQty' value='$product_quantity'>";
+							}
+							
+							/*foreach($_SESSION['cart'] as $key => $value)
+							{
+								echo "<p> Product Name: ".$value['item_name']."</p>";
+								echo "<input class='form-control w-25 d-inline' type='text' name='updateQty' value='".$value['item_quantity']."'>";
+								
+							}*/
+							
+						?>
+						<input type='submit' name='update' value='Update' class='btn btn-warning'>
+					</div>
+					
+				</form>
+				
+				</div>
+				
+			</div>
+			
 		</div>
 	</div>
-	
+	<p><?php echo $status_msg; ?></p>
 	<?php
 		include "includes/footer.php";
 	?>
