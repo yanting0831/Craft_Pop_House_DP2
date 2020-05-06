07204dffbea2f32e9db3a212aa834ffa1ba600f6
Eric1231 <101208805@students.swinburne.edu.my>
2020-05-06 20:11:33 +0800

    Update AdminInterface.php

--- admin/AdminInterface.php
+++ admin/AdminInterface.php
@@ -28,11 +28,14 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-xl-2 col-lg-3 col-md-4 sidebar fixed-top">
+					<!--Title of the sidebar-->
 						<a href="#" class="navbar-brand text-white d-block mx-auto text-center py-3 mb-4 bottom-border">Craft Pop Houses</a>
 					<div class="bottom-border pb-3">
 						<img src="images/admin.jpg" width="50" class="rounded-circle mr-3">
+						
 						<a href="#" class="text-white">Admin</a>
 						</div>
+					<!--sidebar-->
 					<ul class="navbar-nav flex-column mt-4">
 						<li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 current"><i class="fa fa-home text-light fa-lg mr-3"></i>Home</a></li>
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/Category.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-book text-light fa-lg mr-3"></i>Category</a></li>
@@ -40,21 +43,15 @@
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/Admin.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-child text-light fa-lg mr-3"></i>Buyer</a></li>
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/SellerList.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-child text-light fa-lg mr-3"></i>Seller</a></li>						
 						<li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-institution text-light fa-lg mr-3"></i>Transcation List</a></li>						
-						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/slide.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-bell text-light fa-lg mr-3"></i>Edit Announcement</a></li>
+						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/slide.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-bell text-light fa-lg mr-3"></i>Announcement</a></li>
 					</ul>
 					<div class="col-xl-10 col-lg-9 col-md-8 ml-auto bg-dark fixed-top py-2 top-navbar">
 						<div class="row align-items-center">
 							<div class="col-md-4">
-								<h4 class="text-light text-uppercase mb-0">Home</h4>
+								
 							</div>
-							<div class="col-md-5">
-								<form>
-								<div class="input-group">
-									<input type="text" class="form-control " placeholder="Search">
-									<button type="button" class="btn btn-white "><i class="fa fa-search text-danger"></i></button>
-								</div>
-								</form>
-								</div>
+							<div class="col-md-5"></div>
+							<!--logout button-->
 								<div class="col-md-3">
 									<ul class="navbar-nav">
 										<li class="nav-item ml-auto"><a href="../logout.php" class="nav-link"><i class="fa fa-sign-out text-danger fa-lg"></i></a></li>
@@ -62,14 +59,6 @@
 								</div>
 							</div>
 					</div>
-					
-					<div class="container-fluid bg">
-						<div class="row">
-							<div class="col-md-4">
-							
-				</div>
-			</div>
-			</div>
 		</div>
 	</div>
 	</div>
