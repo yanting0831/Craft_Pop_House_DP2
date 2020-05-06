c1a1a5e84a2b4aaf9f9ccf16b1f92985823d1c7e
Eric1231 <101208805@students.swinburne.edu.my>
2020-05-06 20:11:31 +0800

    Update Admin.php

--- admin/Admin.php
+++ admin/Admin.php
@@ -11,35 +11,6 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-	  <form action="register_edit.php" method="POST">
-		<div class="modal-body">
-		  <div class="form-group">
-			<label>Username</label>
-			<input type="text" name="username" placeholder="Enter Username" class="form-control">
-		  </div>
-		  <div class="form-group">
-			<label>Email address</label>
-			<input type="email" name="email" placeholder="Enter Email-Address" class="form-control">
-		  </div>
-		  <div class="form-group">
-			<label>User_type</label>
-			<input type="text" name="user_type" placeholder="Enter user_type" class="form-control">
-		  </div>
-		  <div class="form-group">
-			<label>Password</label>
-			<input type="password" name="password" placeholder="Enter Password" class="form-control">
-		  </div>
-		  <div class="form-group">
-			<label>Confirm_Password</label>
-			<input type="password" name="confirmpassword" placeholder="Enter Confirm Password" class="form-control">
-		  </div>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit"  name="registerbtn" class="btn btn-primary">Save</button>
-      </div>
-	  </form>
     </div>
   </div>
 </div> 
@@ -49,24 +20,10 @@
 		<div class="col-md-2"></div>
 			<div class="col-md-10 bg">
 				<div class="card shadow mb-4 ">
-					
 					<div class="card-body">
-					
-					<?php
-					if(isset($_SESSION['success']) && $_SESSION['success']!='')
-					{
-						echo '<h2 class="bg-primary text-white"> '.$_SESSION['success'].' </h2>';
-						unset($_SESSION['success']);
-					}
-					if(isset($_SESSION['status']) && $_SESSION['status']!='')
-					{
-						echo '<h2 class="bg-danger text-white">'.$_SESSION['status'].' </h2>';
-						unset($_SESSION['status']);
-					}
-					?>
-					
 						<div class="table-responsive">
 						
+						<!--Retreive data from database-->
 						<?php
 							
 							$connection = mysqli_connect('localhost','root','','cph');
@@ -90,6 +47,8 @@
 								</tr>
 							  </thead>
 							  <tbody>
+							  <!--Show the Data from database when the user_type is Usser-->
+							  <!--check if data is present in the database or not-->
 							  <?php
 							  if(mysqli_num_rows($query_run)>0)
 							  {
