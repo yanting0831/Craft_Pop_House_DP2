1f15c49586f84b78fc2d3ae049fe36d7944bfb66
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-07 08:12:14 +0800

    Update products.php

--- products.php
+++ products.php
@@ -152,41 +152,7 @@
 									$product_price = $row_products['product_price'];
 									$product_image = $row_products['product_img'];
 									
-									echo "
-										<div class='col-md-4 col-sm-6 center-responsive'>
-											<form action='products.php' method='post'>
-												<div class='product'>
-													<a href = '#?id=<?php echo $product_id ?>'>
-														<img class='img-responsive' src='images/$product_image'>
-													</a>
-													<div class='text'>
-														<h3> 
-															<a href='#?id=<?php echo $product_id ?>'>
-																$product_title
-															</a>
-														<h3>
-														<p class='price'>
-															$$product_price
-														</p>
-														<p class='button'>
-															<input name=\"quantity\" type=\"text\" value=\"1\" class=\"form-control w-25 d-inline\">
-
-															<!--a class='btn btn-default' href='details.php?id=<?php echo $product_id ?-->
-
-															<a class='btn btn-default' href='#'>
-															View Details
-															</a>
-															<button type='submit' class='btn btn-primary' name='add'>Add to Cart <i class='fas fa-shopping-cart'></i></button>
-															<input type='hidden' name='product_id' value='$product_id'>
-															<input type='hidden' name='hidden_price' value='$product_price'>
-															<input type='hidden' name='hidden_name' value='$product_title'>
-														</p>
-														
-													</div>
-												</div>
-											</form>
-										
-									</div>";
+									component($row_products['product_title'], $row_products['product_price'],$row_products['product_img'], $row_products['product_id']);
 										
 								}
 							}
