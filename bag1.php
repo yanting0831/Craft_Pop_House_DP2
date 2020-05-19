8257678459075989ad7fd7834995a39ef5b516a9
Yan Ting <101209183@students.swinburne.edu.my>
2020-05-19 21:34:05 +0800

    Update bag1.php

--- bag1.php
+++ bag1.php
@@ -1,3 +1,53 @@
+<?php
+	include("function.php");
+	
+	session_start();
+	
+	require_once('includes/component.php');
+	
+	if (isset($_POST['add']))
+	{
+		//print_r($_POST['product_id']);
+		//print_r($_POST['quantity']);
+		
+		if(isset($_SESSION['cart'])){
+
+			$item_array_id = array_column($_SESSION['cart'], "product_id");
+
+			if(in_array($_POST['product_id'], $item_array_id)){
+				echo "<script>alert('Product is already added in the cart..!')</script>";
+				echo "<script>window.location = 'products.php'</script>";
+			}
+			else{
+
+				$count = count($_SESSION['cart']);
+				$item_array = array(
+					'product_id' => $_POST['product_id'],
+					'item_name' => $_POST["hidden_name"],
+					'item_quantity' => $_POST['quantity'],
+					'product_price' => $_POST['hidden_price']
+				);
+
+				$_SESSION['cart'][$count] = $item_array;
+			}
+    }
+	else{
+
+        $item_array = array(
+			'product_id' => $_POST['product_id'],
+			'item_name' => $_POST["hidden_name"],
+			'item_quantity' => $_POST['quantity'],
+			'product_price' => $_POST['hidden_price']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
