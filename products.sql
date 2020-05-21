428d881784ccef026fee49ca2020e6a4456b8c7f
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-21 16:08:48 +0800

    Update products.sql

--- products.sql
+++ products.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2020 at 04:34 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.1.27
+-- Generation Time: May 21, 2020 at 10:08 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,7 +34,6 @@ CREATE TABLE `products` (
   `product_title` text NOT NULL,
   `product_img` text NOT NULL,
   `product_price` text NOT NULL,
-  `product_quantity` int(100) NOT NULL,
   `product_description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,23 +41,23 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `product_category_id`, `product_title`, `product_img`, `product_price`, `product_quantity`, `product_description`) VALUES
-(1, 1, 'Handmade Rattan Handbag', 'bag-1.jpg', '50.00', 1, 'ASD'),
-(2, 1, 'Handmade Rattan Crossbody', 'bag-2.jpg', '45.00', 1, 'ASD'),
-(3, 1, 'Handmade Top-Handle Bag', 'bag-3.jpg', '40.00', 1, 'ASD'),
-(4, 1, 'Handmade Sling Bag', 'bag-4.jpg', '35.00', 1, 'ASD'),
-(5, 3, 'All is Bright Jar Candle', 'candle-1.jpg', '45.00', 1, 'ASD'),
-(6, 3, 'Aloe Water Jar Candle', 'candle-2.jpg', '45.00', 1, 'ASD'),
-(7, 3, 'Alpine Jar Candle', 'candle-3.jpg', '45.00', 1, 'ASD'),
-(8, 3, 'A Calm & Quiet Place Jar ', 'candle-4.jpg', '45.00', 1, 'ASD'),
-(9, 2, 'Clean Charcoal Soap', 'soap-1.jpg', '15.00', 1, 'ASD'),
-(10, 2, 'Apricot Scrub Soap', 'soap-2.jpg', '25.00', 1, 'ASD'),
-(11, 2, 'Deep Clean Soap', 'soap-3.jpg', '22.00', 1, 'ASD'),
-(12, 2, 'Lavender Oil Soap', 'soap-4.jpg', '20.00', 1, 'ASD'),
-(13, 4, 'The Lion King', 'wood-1.jpg', '150.00', 1, 'ASD'),
-(14, 4, 'The Wolves', 'wood-2.jpg', '120.00', 1, 'ASD'),
-(15, 4, 'The Owl', 'wood-3.jpg', '95.00', 1, 'ASD'),
-(16, 4, 'The Grand Piano', 'wood-4.jpg', '85.00', 1, 'ASD');
+INSERT INTO `products` (`product_id`, `product_category_id`, `product_title`, `product_img`, `product_price`, `product_description`) VALUES
+(1, 1, 'Handmade Rattan Handbag', 'bag-1.jpg', '50.00', 'ASD'),
+(2, 1, 'Handmade Rattan Crossbody', 'bag-2.jpg', '45.00', 'ASD'),
+(3, 1, 'Handmade Top-Handle Bag', 'bag-3.jpg', '40.00', 'ASD'),
+(4, 1, 'Handmade Sling Bag', 'bag-4.jpg', '35.00', 'ASD'),
+(5, 3, 'All is Bright Jar Candle', 'candle-1.jpg', '45.00', 'ASD'),
+(6, 3, 'Aloe Water Jar Candle', 'candle-2.jpg', '45.00', 'ASD'),
+(7, 3, 'Alpine Jar Candle', 'candle-3.jpg', '45.00', 'ASD'),
+(8, 3, 'A Calm & Quiet Place Jar ', 'candle-4.jpg', '45.00', 'ASD'),
+(9, 2, 'Clean Charcoal Soap', 'soap-1.jpg', '15.00', 'ASD'),
+(10, 2, 'Apricot Scrub Soap', 'soap-2.jpg', '25.00', 'ASD'),
+(11, 2, 'Deep Clean Soap', 'soap-3.jpg', '22.00', 'ASD'),
+(12, 2, 'Lavender Oil Soap', 'soap-4.jpg', '20.00', 'ASD'),
+(13, 4, 'The Lion King', 'wood-1.jpg', '150.00', 'ASD'),
+(14, 4, 'The Wolves', 'wood-2.jpg', '120.00', 'ASD'),
+(15, 4, 'The Owl', 'wood-3.jpg', '95.00', 'ASD'),
+(16, 4, 'The Grand Piano', 'wood-4.jpg', '85.00', 'ASD');
 
 --
 -- Indexes for dumped tables
