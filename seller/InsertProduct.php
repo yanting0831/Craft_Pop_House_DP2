31c75b8a9d39aa076eb0a4e9a22c1f9c4dc7c40c
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-24 18:49:40 +0800

    Update InsertProduct.php

--- seller/InsertProduct.php
+++ seller/InsertProduct.php
@@ -1,43 +1,48 @@
 <?php
 	$connection = mysqli_connect('localhost','root','','cph');	
+	include("../functions.php");
 ?>
 <?php include('sellerInterface.php') ?>
 	<div class="row">
     
     <div class="col-md-4"></div>
 	<div class="col-md-8 bg">
+		 <?php 
+				
+				require_once('../includes/DbConnect.php');
+				$db   = new DbConnect();
+				$conn = $db->connect();
+
+				require '../classes/users.class.php';
+				$objUser = new user($conn);
+				$objUser->setEmail($_SESSION['user']['email']);
+				$user = $objUser->getUserByEmailId();
+				$_SESSION['sid'] = $user['id'];
+				//print_r ($_SESSION['sid']);
+				//print_r($cartItems);
+				
+			?>
         
-        <div class="panel panel-default">
-            
-           <div class="panel-heading">
-               
+        <div class="panel panel-default">     
+           <div class="panel-heading"> 
                <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Insert Product </h3>
            </div>
            
            <div class="panel-body">
-               
                <form method="post" class="form-horizontal" enctype="multipart/form-data">
-                   
                    <div class="form-group">
-                       
-                      <label class="col-md-3 control-label"> Product Title </label> 
-                      
+				   <input type="hidden" name="seller_id" value="<?= $_SESSION['sid']?>">
+                      <label class="col-md-3 control-label"> Product Title </label>
                       <div class="col-md-6">
-                          
                           <input name="product_title" type="text" class="form-control" required>
-                          
                       </div>
                        
                    </div>
                    
                    <div class="form-group">
-                       
-                      <label class="col-md-3 control-label"> Product Category </label> 
-                      
+                      <label class="col-md-3 control-label"> Product Category </label>
                       <div class="col-md-6">
-                          
-                          <select name="product_category" class="form-control"><!-- form-control Begin -->
-                              
+                          <select name="product_category" class="form-control"><!-- form-control Begin --> 
                               <option> Select a Category Product </option>
                               
                               <?php 
@@ -59,34 +64,23 @@
                       </div>
                    </div>
                    <div class="form-group">
-                       
-                      <label class="col-md-3 control-label"> Product Image </label> 
-                      
-                      <div class="col-md-6">
-                          
-                          <input name="product_image" type="file" class="form-control" required>
-                          
+                      <label class="col-md-3 control-label"> Product Image </label>    
+                      <div class="col-md-6">   
+                          <input name="product_image" type="file" class="form-control" required>      
                       </div>
                        
                    </div>
                                     
                    
-                   <div class="form-group">
-                       
+                   <div class="form-group">       
                       <label class="col-md-3 control-label"> Product Price </label> 
-                      
-                      <div class="col-md-6">
-                          
-                          <input name="product_price" type="text" class="form-control" required>
-                          
+                      <div class="col-md-6"> 
+                          <input name="product_price" type="text" class="form-control" required>      
                       </div>
-                       
                    </div>
                                      
-                   <div class="form-group">
-                       
+                   <div class="form-group"> 
                       <label class="col-md-3 control-label"> Product Desc </label> 
-                      
                       <div class="col-md-6">   
                           <textarea name="product_description" cols="19" rows="6" class="form-control"></textarea>  
                       </div>  
@@ -129,6 +123,7 @@
 <?php
 	if(isset($_POST['submit']))
 	{
+		$seller_id = $_POST['seller_id'];
 		$product_title=$_POST['product_title'];
 		$product_category=$_POST['product_category'];
 		$product_price=$_POST['product_price'];
@@ -138,8 +133,8 @@
 		
 		move_uploaded_file($temp_name,"images/$product_image");
 		$insert_product = "insert into products
-		(product_category_id,product_title,product_img,product_price,product_description) values
-		('$product_category','$product_title','$product_image','$product_price','$product_description')";
+		(seller_id, product_category_id,product_title,product_img,product_price,product_description) values
+		('$seller_id','$product_category','$product_title','$product_image','$product_price','$product_description')";
 		
 		$run_product= mysqli_query($connection,$insert_product);
 		
