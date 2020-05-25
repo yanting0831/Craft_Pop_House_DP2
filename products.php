005c7d18557fa26e92d48a3c1df4fe59ae78798d
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-25 19:33:33 +0800

    Update products.php

--- products.php
+++ products.php
@@ -6,6 +6,64 @@
 	
 	require_once('includes/component.php');
 	
+	$conn = new mysqli('localhost', 'root', '', 'cph');
+	
+	function createCommentRow($data) {
+		global $conn;
+
+		$response = '
+				<div class="comment">
+					<div class="user">'.$data['username'].' <span class="time timeago" data-date="'.$data['createdOn'].'"></span></div>
+					<div class="userComment">'.$data['comment'].'</div>
+					<div class="reply"><a href="javascript:void(0)" data-commentID="'.$data['id'].'" onclick="reply(this)">REPLY</a></div>
+					<div class="replies">';
+
+		$sql = $conn->query("SELECT replies.id, username, comment, replies.createdOn FROM replies INNER JOIN users ON replies.userID = users.id WHERE replies.commentID = '".$data['id']."' ORDER BY replies.id DESC LIMIT 1");
+		while($dataR = $sql->fetch_assoc())
+			$response .= createCommentRow($dataR);
+
+		$response .= '
+							</div>
+				</div>
+			';
+
+		return $response;
+	}
+	
+	if (isset($_POST['getAllComments'])) {
+		$start = $conn->real_escape_string($_POST['start']);
+
+		$response = "";
+		$sql = $conn->query("SELECT comments.id, username, comment, comments.createdOn FROM comments INNER JOIN users ON comments.userID = users.id ORDER BY comments.id DESC LIMIT $start, 20");
+		while($data = $sql->fetch_assoc())
+			$response .= createCommentRow($data);
+
+		exit($response);
+	}
+	
+	if (isset($_POST['addComment'])) {
+        $comment = $conn->real_escape_string($_POST['comment']);
+		$isReply = $conn->real_escape_string($_POST['isReply']);
+		$commentID = $conn->real_escape_string($_POST['commentID']);
+		
+		if($isReply != 'false')
+		{
+			$conn->query("INSERT INTO replies (commentID, comment, createdOn, userID) VALUES ('$commentID', '$comment', NOW(), '".$_SESSION['user']['id']."')");
+			$sql = $conn->query("SELECT replies.id, username, comment, replies.createdOn FROM replies INNER JOIN users ON replies.userID = users.id ORDER BY replies.id DESC LIMIT 1");
+		}
+		else
+		{
+			$conn->query("INSERT INTO comments (userID, comment, createdOn) VALUES ('".$_SESSION['user']['id']."','$comment',NOW())");
+			$sql = $conn->query("SELECT comments.id, username, comment, comments.createdOn FROM comments INNER JOIN users ON comments.userID = users.id ORDER BY comments.id DESC LIMIT 1");
+		}
+
+        
+        $data = $sql->fetch_assoc();
+		exit(createCommentRow($data));
+    }
+	
+$sqlNumComments = $conn->query("SELECT id FROM comments");
+$numComments = $sqlNumComments->num_rows;
 ?>
 
 <!DOCTYPE html>
@@ -30,9 +88,31 @@
     	.alert, #loader {
     	display: none;
 		}
-		.glyphicon, #itemCount {
-    	font-size: 18px;
-		}
+		.comment {
+            margin-bottom: 20px;
+        }
+
+        .user {
+            font-weight: bold;
+            color: black;
+        }
+
+        .time, .reply {
+            color: gray;
+        }
+
+        .userComment {
+            color: #000;
+        }
+
+        .replies .comment {
+            margin-top: 20px;
+
+        }
+
+        .replies {
+            margin-left: 20px;
+        }
     }
     </style>
 </head>
@@ -212,19 +292,139 @@
 						
 				</div>
 				
+				<!--user comments-->
+				
 				
 				</div>		
 				
 			</div>	
 			
-		</div>
-		
-	</div>
+			<!--user comments-->
+			<div class="container" style="margin-top:50px;">
+				<div class="row">
+					<h1> Comment Section </h1>
+				</div>
+			
+				<div class="row">
+					<div class="col-md-12">
+						<textarea class="form-control" id="mainComment" placeholder="Add Public Comment" cols="30" rows="2"></textarea><br>
+						<button style="float:right" class="btn-primary btn" onclick="isReply = false;" id="addComment">Add Comment</button>
+					</div>
+				</div>
+			
+				<div class="row">
+					<div class="col-md-12">
+						<h2><b id="numComments"><?php echo $numComments ?> Comments</b></h2>
+						<div class="userComments">
+
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row replyRow" style="display:none">
+				<div class="col-md-12">
+					<textarea class="form-control" id="replyComment" placeholder="Add Public Comment" cols="30" rows="2"></textarea><br>
+					<button style="float:right" class="btn-primary btn" onclick="isReply = true;" id="addReply">Add Reply</button>
+					<button style="float:right" class="btn-default btn" onclick="$('.replyRow').hide();">Close</button>
+				</div>
+			</div>
+				</div>
+				
+			</div>
 
 
 	<?php
 		include "includes/footer.php";
 	?>
+	
+	<script src="http://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	<script src="includes/jquery.timeago.js"></script>
+	
+	<script type="text/javascript">
+		var isReply = false, commentID = 0, max = <?php echo $numComments ?>;
+		
+        $(document).ready(function () {
+           $("#addComment, #addReply").on('click', function () {
+				var comment;
+
+				if (!isReply)
+					comment = $("#mainComment").val();
+				else
+					comment = $("#replyComment").val();
+
+			   if (comment.length > 5) {
+					$.ajax({
+						url: 'products.php',
+						method: 'POST',
+						dataType: 'text',
+						data: {
+							addComment: 1,
+							comment: comment,
+							isReply: isReply,
+							commentID: commentID
+						}, success: function (response) {
+							max++;
+							$("#numComments").text(max + " Comments");
+							
+							if (!isReply) {
+								$(".userComments").prepend(response);
+								$("#mainComment").val("");
+							} else {
+								commentID = 0;
+								$("#replyComment").val("");
+								$(".replyRow").hide();
+								$('.replyRow').parent().next().append(response);
+							}
+							
+							calcTimeAgo();
+						}
+						
+					});
+			   } else
+				   alert('Please Check Your Inputs');
+		});
+		   
+		   getAllComments(0, max);
+        });
+		
+		function reply(caller) {
+			commentID = $(caller).attr('data-commentID');
+			$(".replyRow").insertAfter($(caller));
+			$('.replyRow').show();
+		}
+		
+		function calcTimeAgo() {
+			$('.timeago').each(function () {
+				var timeAgo = $.timeago($(this).attr('data-date'));
+				$(this).text(timeAgo);
+				$(this).removeClass('timeago');
+			});
+		}
+	
+		function getAllComments(start, max) {
+			if (start > max) {
+				calcTimeAgo();
+				return;
+			}
+
+			$.ajax({
+				url: 'products.php',
+				method: 'POST',
+				dataType: 'text',
+				data: {
+					getAllComments: 1,
+					start: start
+				}, success: function (response) {
+					$(".userComments").append(response);
+					getAllComments((start+20), max);
+				}
+			});
+		}
+    </script>
 </body>
 
 <script type="text/javascript">
