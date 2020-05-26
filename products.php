5edef9edfbc05070778f27bea00f9234a0549e9a
Ricky Su <100083700@students.swinburne.edu.my>
2020-05-26 10:16:54 +0800

    Update products.php

--- products.php
+++ products.php
@@ -4,8 +4,6 @@
 	//error_reporting(0);
 	//ini_set('display_errors', 0);
 	
-	require_once('includes/component.php');
-	
 	$conn = new mysqli('localhost', 'root', '', 'cph');
 	
 	function createCommentRow($data) {
