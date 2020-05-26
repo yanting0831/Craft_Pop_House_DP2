1bd05f5491f69b23a43e27f63f80fa3583b7cdd5
Yan Ting <101209183@students.swinburne.edu.my>
2020-05-26 22:46:02 +0800

    Update events.php

--- events.php
+++ events.php
@@ -40,5 +40,13 @@
 		include "includes/footer.php";
 	?>
 </body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.14.5/firebase-app.js"></script>
 
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.14.5/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 </html>
