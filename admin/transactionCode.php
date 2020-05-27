e89362e8be83687bdc74ba22c6195812592cea46
Eric1231 <55660140+Eric1231@users.noreply.github.com>
2020-05-27 19:16:48 +0800

    Create transactionCode.php

---
+++ admin/transactionCode.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+
+	$connection = mysqli_connect('localhost','root','','cph');	
+
+	
+	if(isset($_POST['delete_btn']))
+		
+		{
+			$id=$_POST['delete_id'];
+			
+			$query = "DELETE FROM orders WHERE invoice_no='$id'";
+			$query_run = mysqli_query($connection, $query);
+			
+			if($query_run)
+			{
+				$_SESSION['success']="Your Data is Deleted";
+				header('Location:transcation.php');
+			}
+			else{
+				$_SESSION['status']="Your Data is Not Deleted";
+				header('Location:transcation.php');
+			}
+		}
+
+?>
