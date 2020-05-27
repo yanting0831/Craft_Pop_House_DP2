cd15ed3bbff0cb7e9b5cb20b7fdb75f72416cac7
Eric1231 <55660140+Eric1231@users.noreply.github.com>
2020-05-27 19:17:36 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -71,12 +71,16 @@
 
 			<p class="subscribe">Subscribe to receive latest updates and news.</p>
 			
-			
-			<input type="text" class="form-control" placeholder="Your email"
-			aria-label="Your email" aria-describedby="basic-addon2">
-			<button class="btn btn-primary" id="submit-footer" type="submit">Submit</button>
-			
+			<form action="index.php" method="post">
 
+				<input type="text" name="f_name" class="form-control" placeholder="Your first name" aria-label="Your first name" aria-describedby="basic-addon2"/>
+				<input type="text" name="mail" class="form-control" placeholder="Your email" aria-label="Your email" aria-describedby="basic-addon2"/>
+
+
+				<input type="submit" name="subscribe" class="btn btn-primary" id="submit-footer"/>	
+
+				<!--<button class="btn btn-primary" id="submit-footer" type="submit">Submit</button>-->
+			</form>
 		  </div>
 		  <!-- Grid column -->
 
