cbcbf4de5faa3cfdb42aea8a4e0e3a6e785be175
Yan Ting <101209183@students.swinburne.edu.my>
2020-05-28 02:00:37 +0800

    Update products.sql

--- products.sql
+++ products.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 26, 2020 at 04:42 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: May 27, 2020 at 05:37 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,22 +42,21 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `seller_id`, `product_category_id`, `product_title`, `product_img`, `product_price`, `product_description`) VALUES
-(24, 16, 1, 'Handmade Rattan Handbag', 'bag-1.jpg', '50.00', 'ASD'),
-(26, 2, 1, 'Handmade Rattan Crossbody', 'bag-2.jpg', '45.00', 'ASD'),
-(27, 2, 1, 'Handmade Top-Handle Bag', 'bag-3.jpg', '40.00', 'ASD'),
-(28, 2, 1, 'Handmade Sling Bag', 'bag-4.jpg', '35.00', 'ASD'),
-(29, 2, 3, 'All is Bright Jar Candle', 'candle-1.jpg', '45.00', 'ASD'),
-(30, 17, 3, 'Aloe Water Jar Candle', 'candle-2.jpg', '45.00', 'ASD'),
-(31, 17, 3, 'Alpine Wooden Jar Candle', 'candle-3.jpg', '45.00', 'ASD'),
-(32, 17, 3, 'A Calm & Quiet Place Jar', 'candle-4.jpg', '45.00', 'ASD'),
-(33, 16, 2, 'Wood Coal Soap', 'soap-1.jpg', '15.00', 'ASD'),
-(34, 16, 2, 'Apricot Scrub Soap', 'soap-2.jpg', '25.00', 'ASD'),
-(35, 16, 2, 'Deep Clean Soap', 'soap-3.jpg', '22.00', 'ASD'),
-(36, 16, 2, 'Lavender Oil Soap', 'soap-4.jpg', '20.00', 'ASD'),
-(37, 16, 4, 'The Lion King', 'wood-1.jpg', '150.00', 'ASD'),
-(38, 16, 4, 'The Wolves', 'wood-2.jpg', '120.00', 'ASD'),
-(39, 16, 4, 'The Owl', 'wood-3.jpg', '95.00', 'ASD'),
-(40, 16, 4, 'The Grand Piano', 'wood-4.jpg', '85.00', 'ASD');
+(1, 2, 1, 'Handmade Woven Sling Bag', 'bag-4.jpg', '35', 'This bag designed by Craft Pop House allows you to bring everything with you whenever you need to. Bring this back home, bring it for travelling, shopping or any leisure activities to inspire your friends and family with this casual and nice bag.'),
+(2, 2, 1, 'Handmade Top-Handle Bag', 'bag-3.jpg', '40', 'This traditional rattan made handbag allows you to bring everything in this simplistic and fashion design bag. Get compliments from everyone on the gorgeous gold hardware on this stunning bag.'),
+(3, 2, 1, 'Handmade Rattan Crossbody Bag', 'bag-2.jpg', '45', 'This beautiful rattan crossbody bag by Craft Pop House allows you to bring everything with you whenever you need to, but fashionably of course! Get compliments from everyone on the gorgeous gold hardware on this stunning bag.'),
+(4, 2, 3, 'All is Bright Jar Candle', 'candle-1.jpg', '45', 'A blend of sparkling citrus scents drifting on warm musk.'),
+(5, 2, 3, 'Aloe Water Jar Candle', 'candle-2.jpg', '45', 'A sophisticated and soothing blend of florals and subtle fruits. \r\n		Fragrance Notes:\r\n		Top: Sheer Citrus, Aldehydes, Greens\r\n		Mid: Mimosa, Ylang, Jasmine, Rose, Tuberose\r\n		Base: Sweet Musk, Sandalwood, Tonka Bean\r\n		Burn Time:110 to 150 hours\r\n		Weight:22oz'),
+(6, 2, 3, 'A Calm Jar Candle', 'candle-4.jpg', '55', 'A meditative fragrance with gentle jasmine, patchouli and amber musk.'),
+(7, 2, 3, 'Alpine Morning Jar Candle', 'candle-3.jpg', '45', 'Wake up to a breath of fresh mountain air: flowers and spice, with a zing of citrus.'),
+(8, 2, 2, 'Charcoal Soap', 'soap-1.jpg', '15', 'Charcoal detoxifies skin by absorbing oil and impurities that cause acne and blackheads. Shea butter, castor oil & wheat germ oil help to soothen skin and promote scar healing especially for acne. Suitable for oily skin with pimples and acne.'),
+(9, 2, 2, 'Kernel Scrub Soap', 'soap-2.jpg', '25', 'A soap which gently exfoliates with apricot kernel powder and is not stripping on the skin. Enjoy it daily with the warm and relaxing scent of lemongrass and rosemary essential oils.'),
+(10, 2, 2, 'Chamomile Soap', 'soap-3.jpg', '22', 'Chamomile is traditionally used to relieve skin irritations such as acne, insect bites and itching. To fully distill the benefits of the chamomile flower, we have infused chamomile flowers in olive oil for 2 months before using the oil for soap making.  Suitable for sensitive dry skin.'),
+(11, 2, 2, 'Lavender Olive Soap', 'soap-4.jpg', '20', 'To make this bar, we infused lavender flowers into olive oil for 2 months. This soap is also superfatted with evening primrose oil to help with skin inflammations, acne and itching.This soap is suitable for normal, sentisive and dry skin types.'),
+(12, 2, 4, 'The Grand Piano', 'wood-4.jpg', '85', 'The art pieces are all hand assembled with the finest wood veneer.'),
+(13, 2, 4, 'The Lion King', 'wood-1.jpg', '150', 'The art pieces are all hand assembled with the finest wood veneer.'),
+(14, 2, 4, 'The Wolves', 'wood-2.jpg', '120', 'The art pieces are all hand assembled with the finest wood veneer.'),
+(15, 2, 4, 'The Owl', 'wood-3.jpg', '95', 'The art pieces are all hand assembled with the finest wood veneer.');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +76,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
