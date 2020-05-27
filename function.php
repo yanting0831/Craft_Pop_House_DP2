f346b2c1f5f7dc49ed4b88d32f0affed285eca33
Eric1231 <55660140+Eric1231@users.noreply.github.com>
2020-05-27 19:17:33 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,63 @@
 <?php 
+	include('db.php');
+	include "includes/nav_header.php";
+?>
 
-	$connection=mysqli_connect('localhost','root','','cph');	
+<?php 
+	
+	function getRealIpUser(){
+    
+    switch(true){
+            
+            case(!empty($_SERVER['HTTP_X_REAL_IP'])) : return $_SERVER['HTTP_X_REAL_IP'];
+            case(!empty($_SERVER['HTTP_CLIENT_IP'])) : return $_SERVER['HTTP_CLIENT_IP'];
+            case(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) : return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            
+            default : return $_SERVER['REMOTE_ADDR'];
+            
+    }
+    
+}
+
+/// begin add_cart functions ///
+
+function add_cart(){
+    
+	
+    global $connection;
+    
+    if(isset($_GET['add_cart'])){
+		// print_r("Hekko");
+        
+        $ip_add = getRealIpUser();
+        
+        $p_id = $_GET['add_cart'];
+        
+        $product_quantity = $_POST['product_qty'];
+     
+        
+        $check_product = "select * from carts where ip_add='$ip_add' AND p_id='$p_id'";
+        
+        $run_check = mysqli_query($connection,$check_product);
+        
+        if(mysqli_num_rows($run_check)>0){
+            
+            echo "<script>alert('This product has already added in cart')</script>";
+            echo "<script>window.open('details.php?product_id=$p_id','_self')</script>";
+            
+        }else{
+            
+            $query = "insert into carts (p_id,ip_add,qty) values ('$p_id','$ip_add','$product_quantity')";
+            
+            $run_query = mysqli_query($connection,$query);
+            
+            echo "<script>window.open('details.php?product_id=$p_id','_self')</script>";
+            
+        }
+        
+    }
+    
+}
 	
 	function getPro()
 	{
@@ -21,15 +78,16 @@
 			
 			<div class='col-md-4 col-sm-6 center-responsive'>
 				<div class='product'>
-					<a href = '#'>
-						<img class='img-responsive' src='images/$product_image'>
+					<a href='detail.php?product_id=$product_id'>
+						<img class='img-responsive' src='images/$product_image/$product_image'>
 					</a>
 					
 					<div class='text'>
 						
 						<h3> 
-							<a href='#'>
+							<a href='detail.php?product_id=$product_id'>
 								$product_title
+								
 							</a>
 						
 						<h3>
@@ -39,11 +97,7 @@
 						</p>
 						
 						<p class='button'>
-							<a class='btn btn-default' href='#'>
-								View Details
-							</a>
-							
-							<a class='btn btn-primary' href='#'>
+							<a class='btn btn-primary' href='detail.php?product_id=$product_id'>
 								<i class='fa fa-shopping-cart'></i>Add to Cart
 							</a>
 						</p>
@@ -82,13 +136,10 @@
 	function getCategoryCO()
 	{
 		global $connection;
-		require_once('includes/DbConnect.php');
-		$db   = new DbConnect();
-		$conn = $db->connect();
 		
 		if(isset($_GET['category']))
 		{
-			$category_id = $_GET['category'];
+			$category_id=$_GET['category'];
 			$get_category_id ="select * from products_categories where product_category_id='$category_id'";
 			$run_category=mysqli_query($connection,$get_category_id);
 			$row_category=mysqli_fetch_array($run_category);
@@ -120,49 +171,66 @@
 			while($row_products=mysqli_fetch_array($run_products))
 			{
 				$product_id = $row_products['product_id'];
-				$product_title = $row_products['product_title'];
-				$product_price = $row_products['product_price'];
-				$product_image = $row_products['product_img'];
-				
-			?>
-				
-					<div class="col-sm-6 col-md-4">
-						<div class="thumbnail">
-						  <img src="images/<?= $row_products['product_img']; ?>" alt="" style="width: 200px; height: 200px;">
-						  <div class="caption">
-							<h3><?= $row_products['product_title']; ?></h3>
-							
-							<?php
-							$db = mysqli_connect('localhost', 'root', '', 'cph');
-							$test = $row_products['seller_id'];
-							$query = "SELECT * FROM users where id='$test'";
-							$results = mysqli_query($db, $query);
-							while ($rows=mysqli_fetch_array($results)){
-								$a = $rows['username'];
-								echo "<p>Seller UserName: $a</p>";
-							} ?>
-							
-							<p>
-								<div class="row">
-									<div class="col-sm-6 col-md-6">
-										<strong> <span style="font-size: 18px;">RM</span><?= number_format( $row_products['product_price'], 2 ); ?></strong>
-									</div>
-									<div class="col-sm-6 col-md-6">
-										<?php	
+										$product_title = $row_products['product_title'];
+										$product_price = $row_products['product_price'];
+										$seller_id= $row_products['seller_id'];
+										$product_image = $row_products['product_img'];
 										
-											$disButton = "";
-											
-										 ?>
-
-										<button id="cartBtn_<?=$row_products['product_id'];?>" <?php echo $disButton; ?> class="btn btn-success" onclick="addToCart(<?=$row_products['product_id'];?>, this.id); check_login(); " role="button">Add To Cart</button>
-									</div>
-								</div>
-							</p>
-						  </div>
-						</div>
-					</div>
-		<?php	}
+										 echo "
+										<div class='col-md-4 col-sm-6 center-responsive'>
+											<div class='product'>
+												<a href='details.php?product_id=$product_id'>
+													<img class='img-responsive' src='images/$product_image'>
+												</a>
+												<div class='text'>
+													<h3> 
+														<a href='details.php?product_id=$product_id'> $product_title
+															
+														</a>
+													<h3>
+													<p>Seller id :
+													$seller_id
+													<p>
+													<p >
+														RM$product_price
+													</p>
+													<p class='button'>
+														<a class='btn btn-default' href='details.php?product_id=$product_id'>
+														View Details
+														</a>
+														<a class='btn btn-primary' href='details.php?product_id=$product_id'>
+															<i class='fa fa-shopping-cart'></i>Add to Cart
+														</a>
+													</p>
+													
+												</div>
+											</div>
+										</div>
+									";
+			
+			
+			}
 		}
-	}	
+	}
+	function subscribe(){
+
+	global $connection;
+
+	if(isset($_POST['subscribe'])){	 
+		 $firstname = $_POST['f_name'];
+		 $mailto = $_POST['mail'];
+		 $sql = "INSERT INTO subscribers (fname, email)
+		 VALUES ('$firstname', '$mailto')";
+		 if (mysqli_query($connection, $sql)) {
+			echo "<script>alert('Subscibe successfully! Thanks for supporting us!');</script>";
+		 } else {
+			// echo "Error: " . $sql . " " . mysqli_error($connection);
+		 }
+		 mysqli_close($connection);
+	}
+}
+	
+
 ?>	
+	
 	
