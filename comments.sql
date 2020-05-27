159c11778108cee249c1fff368e602513b52130a
Yan Ting <101209183@students.swinburne.edu.my>
2020-05-28 02:00:56 +0800

    Update comments.sql

--- comments.sql
+++ comments.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2020 at 08:20 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: May 27, 2020 at 05:35 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,12 +39,7 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `userID`, `comment`, `createdOn`) VALUES
-(21, 13, 'asd124312', '2020-05-25 13:39:04'),
-(22, 13, '333333333333333', '2020-05-25 13:39:14'),
-(23, 13, '12312413', '2020-05-25 13:43:31'),
-(24, 13, '111111111111', '2020-05-25 13:55:45'),
-(25, 13, 'test123', '2020-05-25 13:59:48'),
-(26, 14, 'TestingForDP2', '2020-05-25 14:11:43');
+(28, 20, 'Nice Product. ', '2020-05-27 10:57:45');
 
 --
 -- Indexes for dumped tables
@@ -66,17 +60,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
