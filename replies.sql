70091b73b5938017742bee7429602608c8e63bb0
Yan Ting <101209183@students.swinburne.edu.my>
2020-05-28 02:00:46 +0800

    Update replies.sql

--- replies.sql
+++ replies.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2020 at 08:20 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Generation Time: May 27, 2020 at 05:37 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,18 +34,6 @@ CREATE TABLE `replies` (
   `createdOn` datetime NOT NULL,
   `userID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `replies`
---
-
-INSERT INTO `replies` (`id`, `commentID`, `comment`, `createdOn`, `userID`) VALUES
-(1, 23, 'hahah123', '2020-05-25 13:48:13', 13),
-(2, 22, 'trhhhh', '2020-05-25 13:50:32', 13),
-(3, 21, '44444444', '2020-05-25 13:50:38', 13),
-(4, 24, '123124123', '2020-05-25 13:56:45', 13),
-(5, 25, 'allahu', '2020-05-25 13:59:53', 13),
-(6, 26, 'You suck', '2020-05-25 14:11:54', 13);
 
 --
 -- Indexes for dumped tables
