da47912c94f219ee69589e1750b48556441549a2
Eric1231 <55660140+Eric1231@users.noreply.github.com>
2020-05-27 19:18:23 +0800

    Update SellerProduct.php

--- seller/SellerProduct.php
+++ seller/SellerProduct.php
@@ -84,7 +84,29 @@
 									<td>
 										<form action="SellerProductCode.php" method="post">
 											<input type="hidden" name="delete_id" value="<?php echo $row['product_id']; ?>">
-											<button type="submit" name="delete_btn" class="btn btn-danger">DELETE</button>
+											<button type="button" name="delete_btn" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">DELETE</button>
+											<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+											  <div class="modal-dialog" role="document">
+												<div class="modal-content">
+												  <div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLabel">Confirm to DELETE</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													  <span aria-hidden="true">&times;</span>
+													</button>
+												  </div>
+												  <div class="modal-body">
+													ARE U SURE U WANT TO DELETE.
+												  </div>
+												  <div class="modal-footer">
+													<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+
+													<input type="hidden" name="delete_id" value="<?php echo $row['product_id']; ?>">
+													<button type="submit" name="delete_btn" class="btn btn-danger">DELETE</button>
+											
+													</div>
+												</div>
+											  </div>
+											</div>
 										</form>
 									</td>
 								</tr>
