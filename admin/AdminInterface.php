7be3f48633590088496d08588a13be173bfd23e7
Eric1231 <55660140+Eric1231@users.noreply.github.com>
2020-05-27 19:16:26 +0800

    Update AdminInterface.php

--- admin/AdminInterface.php
+++ admin/AdminInterface.php
@@ -42,7 +42,7 @@
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/Aproduct.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-gift text-light fa-lg mr-3"></i>Product</a></li>
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/Admin.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-child text-light fa-lg mr-3"></i>Buyer</a></li>
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/SellerList.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-child text-light fa-lg mr-3"></i>Seller</a></li>						
-						<li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-institution text-light fa-lg mr-3"></i>Transcation List</a></li>						
+						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/IncomeCPH.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-institution text-light fa-lg mr-3"></i>Transcation List</a></li>						
 						<li class="nav-item"><a href="http://localhost/Craft_Pop_House_DP2/admin/slide.php" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fa fa-bell text-light fa-lg mr-3"></i>Announcement</a></li>
 					</ul>
 					<div class="col-xl-10 col-lg-9 col-md-8 ml-auto bg-dark fixed-top py-2 top-navbar">
